7351935c9354585e9fdc9fbc50023899a30505b9
Benjamin Strauch <benjamin.strauch@vara.ai>
2020-12-02 13:28:29 +0100

    Day1

---
+++ day1.clj
@@ -0,0 +1,14 @@
+(require '[clojure.string :as str])
+
+(def inp1 (mapv #(Integer. %) (str/split (slurp "inputs/input1.txt") #"\n")))
+
+(println 
+  (for 
+    [i (range (count inp1)) 
+     j (range i) 
+     k (range j) 
+     :let [x (nth inp1 i) 
+           y (nth inp1 j) 
+           z (nth inp1 k)] 
+     :when (== (+ x y z) 2020)] 
+    (* x y z)))
